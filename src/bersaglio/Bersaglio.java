b140f5d671a37afe8119acde77950c47bce986d1
Piergiorgio <piergiorgio.bertolini@marconirovereto.it>
2018-10-18 12:03:35 +0200

    Animazione di Bersaglio

--- src/bersaglio/Bersaglio.java
+++ src/bersaglio/Bersaglio.java
@@ -2,30 +2,57 @@ package bersaglio;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
 
-public class Bersaglio extends JPanel {
+public class Bersaglio extends JPanel implements ActionListener {
+
+    private static final int SIZE = 100;
+    private Timer timer = new Timer(50, this);
+    private int x = 0, y = 0, dx = 7, dy = 5;
 
     public Bersaglio() {
-        setBackground(Color.PINK);
-        setPreferredSize(new Dimension(300, 300));
+        //setBackground(Color.PINK);
+        setPreferredSize(new Dimension(SIZE, SIZE));
+        timer.start();
+    }
+
+    public void sposta(int with, int hight) {
+        if (x < 0 || x + SIZE >= with - 4 * dx) {
+            dx = -dx;
+        }
+        if (y < 0 || y + SIZE >= hight - 8 * dy) {
+            dy = -dy;
+        }
+        x += dx;
+        y += dy;
+        setLocation(new Point(x, y));
+        repaint();
     }
 
     // tracciamento della grafica
     public void paintComponent(Graphics page) {
         super.paintComponent(page);
         int cerchi = 6;
-        int x = 0, y = 0, diametro = 300;
-        int delta=diametro/(2*cerchi)-3;
+        int x = 0, y = 0, diametro = SIZE;
+        int delta = diametro / (2 * cerchi) - 1;
         for (int i = 0; i < cerchi; i++) {
-            if (i%2==0)
+            if (i % 2 == 0) {
                 page.setColor(Color.BLUE);
-            else
+            } else {
                 page.setColor(Color.ORANGE);
+            }
             page.fillOval(x, y, diametro, diametro);
-            y=(x+=delta);
-            diametro-=2*delta;
+            y = (x += delta);
+            diametro -= 2 * delta;
         }
         page.setColor(Color.RED);
         page.fillOval(x, y, diametro, diametro);
     }
+
+    public void actionPerformed(ActionEvent e) {
+        sposta(MainBersaglio.SIZEX, MainBersaglio.SIZEY);
+    }
+
 }
--- src/bersaglio/MainBersaglio.java
+++ src/bersaglio/MainBersaglio.java
@@ -1,15 +1,18 @@
 package bersaglio;
     
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class MainBersaglio {
-
+    public static final int SIZEX = 700, SIZEY=500;
     public static void main(String[] args) {
         JFrame frame = new JFrame("Il mio bersaglio");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(SIZEX,SIZEY));
         Bersaglio panel = new Bersaglio();
+
         frame.getContentPane().add(panel);
-        frame.pack();
+        frame.pack();// aggiusta il layout e le dimensioni pref.
         frame.setVisible(true);
     }
 }
